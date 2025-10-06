0d185379e61adac1511bd604ecddd70ff72fc555
annayurevychuniv <146587156+annayurevychuniv@users.noreply.github.com>
2025-10-06 13:49:51 +0300

    Create test0.py

---
+++ test0.py
@@ -0,0 +1,13 @@
+print("Hello, world!")
+
+user_input = input("Enter a number: ")
+print("Result:", int(user_input) * 2)
+
+password = "123456"
+if password == "123456":
+    print("Weak password in code!")
+
+def divide(a, b):
+    return a / b
+
+print(divide(10, 0))
